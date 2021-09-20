8d361d4205001e4b5e533b9fc6915f7a39f25af9
Tiff-C <deguntiffany@gmail.com>
2021-09-20 13:05:51 +0000

    creating a table if it doesn't exist

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -12,12 +12,11 @@ connection = pymysql.connect(host='localhost',
                              db='Chinook')
 
 try:
-    # Run a query
-    with connection.cursor(pymysql.cursors.DictCursor) as cursor:
-        sql = "SELECT * FROM Artist;"
-        cursor.execute(sql)
-        for row in cursor:
-            print(row)
+    with connection.cursor() as cursor:
+        cursor.execute("""CREATE TABLE IF NOT EXISTS
+                          Friends(name char(20), age int, DOB datetime);""")
+        # Note that the above will still display a warning (not error) if the
+        # table already exists
 finally:
     # Close the connection, whether or not the above was successful
     connection.close()
